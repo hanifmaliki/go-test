48072667cf5834f6a1e87a1b06d64a670d655471
Hanif Maliki Dewanto <Hanif.Dewanto@petrosea.com>
2023-02-02 09:15:39 +0700

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hanifmaliki/go-test
+
+go 1.19
+
+require github.com/hanifmaliki/go-module-test/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	hello "github.com/hanifmaliki/go-module-test/v2/pkg/hello-world"
+)
+
+func main() {
+	fmt.Println("Test")
+	hello.Print()
+}
